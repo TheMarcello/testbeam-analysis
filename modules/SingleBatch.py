2c13b178cb31991c2f6248e1d42466a0193b4808
Marcello Emilio Pozzessere RU <mpozzessere@lilo.science.ru.nl>
2025-01-30 16:12:23 +0100

    now the CFD value is set to 70% if the sensor is irradiated, and the option to select only the central area can be changed

--- modules/SingleBatch.py
+++ modules/SingleBatch.py
@@ -1,32 +1,30 @@
+### can be run individually with:
+### python3 -m modules.SingleBatch --batch <_c> 
+### optional flags: --SAVE, --show_plots, --fit_charge, --CFD_matrix
+### warning: --show_plots plots are distorted
+
 import numpy as np # NumPy
 import matplotlib.pylab as plt # Matplotlib plots
-# import matplotlib.patches as mpatches
-# import matplotlib.colors as colors
 import mpl_scatter_density     # density scatter plots
 import pandas as pd # Pandas
-# import uproot
-# import pickle
+
 import logging
 import argparse     # to get arguments from command line executing file.py
-
 import os # read directories etc.
 import sys # to pass the argv to the main() function
-# from scipy.signal import find_peaks, gaussian
-# from scipy.optimize import curve_fit
-# from scipy.stats import gaussian_kde
-# import pylandau
 
 import time
 # from timeout_decorator import timeout
 
 from importlib import reload # to reload modules
-import LoadBatch
-reload(LoadBatch)
-from LoadBatch import *
-from SensorClasses import *
+
+# import LoadBatch
+# reload(LoadBatch)
+from .LoadBatch import *
+from .SensorClasses import Batch, Sensor, Oscilloscope
 
 
-def analysis_batch(this_batch, batch_object, S, n_DUT=None, do_plots=True, show_plot=False, SAVE=True, CFD_comparison=False, fit_charge=False, return_results=True, dir_path=None, ROOT_fit_dir=None, Charge_fit_results="Charge_fit_results"):
+def analysis_batch(this_batch, batch_object, S, n_DUT=None, do_plots=True, show_plot=False, SAVE=True, CFD_comparison=False, fit_charge=False, only_center=True, return_results=True, dir_path=None, ROOT_fit_dir=None, Charge_fit_results="Charge_fit_results"):
     """
     Performs analysis of one batch: one (or more) duts in a single oscilloscope. Plots a lot of different quantities and performs some fits.
     Also returns a dictionary with all the calculations performed. \n
@@ -40,6 +38,7 @@ def analysis_batch(this_batch, batch_object, S, n_DUT=None, do_plots=True, show_
     show_plot:      boolean option to show the plots, could raise warnings for too many figures open (they can be saved if this is false)
     SAVE:           boolean option to save the plots
     CFD_comparison: 3x3 plot of delta_t distributions with different CFD values for the dut and MCP, to compare them
+    CFD_DUT:        change the Constant Fraction Discriminator values for the DUT (default 20%)
     fit_charge:     boolean option to call charge_fit.C to perform the Langau*Gauss fit of the charge, if False the results are read from files saved previously
     return_results: boolean option to return a dictionary of results ### right now this is not useful because the dictionary is created anyways
     dir_path:       path to save all the plots, if not
@@ -78,8 +77,7 @@ def analysis_batch(this_batch, batch_object, S, n_DUT=None, do_plots=True, show_
         use_for_geometry_cut = 'time'
     else:
         use_for_geometry_cut = 'pulseheight' 
-    logging.info(f"in analysis_batch(), analysing Batch: {this_batch}, {S}\n/
-                 bins for pulseHeight minimum: {binning_method}, bins for time: {time_bins}, threshold charge: {threshold_charge}fC, bootstrap: {n_bootstrap}")
+    logging.info(f"in analysis_batch(), analysing Batch: {this_batch}, {S}\n bins for pulseHeight minimum: {binning_method}, bins for time: {time_bins}, threshold charge: {threshold_charge}fC, bootstrap: {n_bootstrap}")
 
 
     ### the table has to be generated (in main()), if they don't exist already
@@ -93,12 +91,14 @@ def analysis_batch(this_batch, batch_object, S, n_DUT=None, do_plots=True, show_
     df = {}  ### having a dictionary in this function is now useless (only one S at a time) but changing everything is not worth the risk
     df[S] = load_batch(this_batch,S)
     ### I can add a check to make sure that batch_object and this_batch are from the same batch
+    if batch_object.batch_number != this_batch:
+        logging.error(f"In analysis_batch(), batch_number: {batch_object.batch_number} and this_batch: {this_batch} DO NOT MATCH")
     logging.info(f'MCP: {batch_object.S[S].channels["Ch1"].voltage} V, angle: {batch_object.angle}°, temperature: {batch_object.temperature:.2f}°C')
     
     ### initilizing the dictionary for the results:
     results_dictionary = {dut:{'comments':set()} for dut in DUTs}
 ### ALL OF THE CUTS
-    ### [ ... if dut in DUTs else None for dut in [1,2,3]], it avoids calculating the cuts for the channels with no dut
+    ### [ ... if dut in DUTs else None for dut in [1,2,3]]  avoids calculating the cuts for the channels with no dut
     geo_cuts = [geometry_mask(df[S], DUT_number=dut, bins=these_bins, bins_find_min=binning_method, use=use_for_geometry_cut)[0] if dut in DUTs else None for dut in [1,2,3]]
     central_sensor_area_cuts = [geometry_mask(df[S], DUT_number=dut, bins=these_bins, bins_find_min=binning_method, only_select='center', use=use_for_geometry_cut)[0] if dut in DUTs else None for dut in [1,2,3]]
     time_cuts = [time_mask(df[S], dut, bins=time_bins, mask=geo_cuts[dut-1], n_bootstrap=False, show_plot=False, savefig=os.path.join(dir_path,f'time_plot_with_geo_cuts_{S}_{this_batch}_DUT{dut}.png'))[0] if dut in DUTs else None for dut in [1,2,3]]
@@ -129,7 +129,7 @@ def analysis_batch(this_batch, batch_object, S, n_DUT=None, do_plots=True, show_
             np.savetxt(os.path.join(dir_path, f"charge_data_all_cuts_{this_batch}_{S}_{dut}.csv"),
                         df[S][f'charge_{dut}'].loc[all_cuts[dut-1]]/my_transimpedance, delimiter=',')
             # os.chdir(os.path.join(ROOT_fit_dir,'..')) ### I shouldn't change folder
-            run_root_string = f'root -b -q "charge_fit.C({this_batch},\\"{S}\\",{dut})"'
+            run_root_string = f'root -b -q "{ROOT_fit_dir}charge_fit.C({this_batch},\\"{S}\\",{dut})"'
             os.system(run_root_string)
             
 ### ALL OF THE PLOTS
@@ -219,16 +219,22 @@ def analysis_batch(this_batch, batch_object, S, n_DUT=None, do_plots=True, show_
                 MCP_resolution, MCP_error = 3.73, 1.33   # 3.73 +/- 1.33 ps
             case other: logging.error(f"in analysis_batch(), incorrect MCP voltage: {other}")
         try:
-            # ### time resolution fit (full area)
-            # time_dict = time_mask(df[S], dut, bins=100, n_bootstrap=n_bootstrap, show_plot=show_plot, mask=np.logical_and(geo_cuts[dut-1], time_fit_cuts[dut-1]),
-            #                       savefig=os.path.join(dir_path,f'time_plot_with_geo_cuts_{S}_{this_batch}_DUT{dut}.png'))[1]
-            ### time resolution fit with central area cuts
-            time_dict = time_mask(df[S], dut, bins=100, n_bootstrap=n_bootstrap, show_plot=show_plot, mask=np.logical_and(central_sensor_area_cuts[dut-1], time_fit_cuts[dut-1]),
+            ### for all sensors that are irradiated set CFD for the DUT to 70%
+            if results_dictionary[dut]['fluence'] != 0:         CFD_DUT = 70
+            elif results_dictionary[dut]['fluence'] == 0:       CFD_DUT = 20
+            else:        logging.error(f"Value of fluence not recognized:{results_dictionary[dut]['fluence']}, unable to set CFD value")
+            ### option to make the geometrical cut be only the central 0.5xo.5mm^2 OR the full surface of the dut
+            if only_center: 
+                this_mask = np.logical_and(central_sensor_area_cuts[dut-1], time_fit_cuts[dut-1])
+            else:
+                this_mask = np.logical_and(geo_cuts[dut-1],time_fit_cuts[dut-1])
+
+            time_dict = time_mask(df[S], dut, bins=100, n_bootstrap=n_bootstrap, show_plot=show_plot, mask=this_mask, CFD_DUT=CFD_DUT,
                                   title_info=' center cut', savefig=os.path.join(dir_path,f'time_plot_with_center_cuts_{S}_{this_batch}_DUT{dut}.png'))[1]
             time_resolution, time_res_err = error_propagation(time_dict['parameters'][2], time_dict['parameters_errors'][2], MCP_resolution, MCP_error)
-        except:
-            logging.error(f"in analysis_batch(), Time fit error")
-            results_dictionary[dut]['comments'].add("Time fit error")
+        except Exception as e:
+            logging.error(f"in analysis_batch(), Time fit error: {e}")
+            results_dictionary[dut]['comments'].add(f"Time fit error ({e})")
             time_resolution, time_res_err = 0, 0
         
         results_dictionary[dut]['time_resolution'], results_dictionary[dut]['time_res_err'] = time_resolution, time_res_err
@@ -256,7 +262,7 @@ def main(argv):
     parser.add_argument('--SAVE', action='store_true', help='SAVE option to save plots (or not)')
     parser.add_argument('--show_plots', action='store_true', help='option to show plots (or not)')
     parser.add_argument('--fit_charge', action='store_true', help='if charge should be fitted (with ROOT)')
-    parser.add_argument('--CFD', action='store_true', help='option for CFD comparison plots')
+    parser.add_argument('--CFD_matrix', action='store_true', help='option for CFD comparison plots')
     # parser.add_argument('-d', default=False, help='Output directory')
     args = parser.parse_args(argv[1:])
 
@@ -264,11 +270,14 @@ def main(argv):
     fit_charge = args.fit_charge
     SAVE = args.SAVE
     show_plot = args.show_plots
-    CFD_comparison = args.CFD
+    CFD_comparison = args.CFD_matrix
     logging.info(f"in main() of Single_batch.py, parsed arguments:  batch: {this_batch}, SAVE: {SAVE}, show_plot: {show_plot}, fit_charge: {fit_charge}, CFD_comp: {CFD_comparison}")
 
+    if this_batch is None:
+        logging.error(f"in main() of Singe_batch, no batch number has been passed as argument, run e.g. 'python3 Single_batch.py --batch 301 --SAVE' ")
+        return 1
     ### Load the dictionary of sensor names and runs
-    dict_of_batches = read_pickle("/home/marcello/Desktop/Radboud_not_synchro/Master_Thesis/testbeam-analysis/dict_of_batches.pickle")
+    dict_of_batches = read_pickle("./files/dict_of_batches.pickle")
     ### I want to keep this specific directory for many reasons (fit charge, all plots etc.)
     dir_path = f'/home/marcello/Desktop/Radboud_not_synchro/Master_Thesis/various plots/all batches/{this_batch}'
 
